2093b85a25f9e0842493acfb80265476879a28f2
funkiv <funkcharles@hotmail.com>
2024-10-15 11:27:24 -0500

    Defining objects, readonly, optional and function parameters

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
-//Always annotate the parameters and return types, that way errors can be known
-
-//Can define default values for parameters
-//This is a slick way of making parameters optional and handling undefined values
-function calculateTax(income: number, taxYear = 2022) {
-        let x
-        if ( taxYear < 2022)
-                return income * 1.2
-        //undefined
-        return income
-}
-
-//You can always overide default parameters by passing them in.
-calculateTax(10_000, 2024)
+let employee: {
+        readonly id: number,
+        name: string,
+        retire: (date: Date) => void
+} = { 
+        id: 1, 
+        name: "Charles", 
+        retire: (date:Date) => {
+                console.log(date)
+        }
+} 
