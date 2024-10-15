11e8050608dd889f84d8d32cfc3775bb12158830
funkiv <funkcharles@hotmail.com>
2024-10-15 12:16:01 -0500

    Optional element access, optional calls

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,15 @@
 "use strict";
-function kgToLbs(weight) {
-    if (typeof weight === 'number') {
-        return weight * 2.2;
-    }
-    else {
-        return parseInt(weight) * 2.2;
-    }
+var _a;
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
 }
-kgToLbs(10);
-kgToLbs('10kg');
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+let customers = [1, 2, 3];
+let message = "";
+message = customers === null || customers === void 0 ? void 0 : customers[10].toString();
+console.log(customers[10]);
+console.log();
+let log = null;
+log === null || log === void 0 ? void 0 : log('a');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
-//Nullable types
-
-function greet(name: string | null| undefined) {
-        if (name)
-                console.log(name.toUpperCase())
-        else
-                console.log("Hola")
+type Customer = {
+        birthday?: Date
 }
 
-greet(undefined)
-greet(null)
+function getCustomer(id: number): Customer | null | undefined {
+        return id === 0 ? null : { birthday: new Date() }
+}
+
+let customer = getCustomer(1)
+//Optional property access operator
+//Gets executed only if customer and birthday is not null or undefined
+        console.log(customer?.birthday?.getFullYear())
+
+//Optional element access operator
+// if (customers !== null && customers !== undefined) essentially
+//CHECKS IF THE ARRAY EXISTS NOT IF THE INDEX IS THERE BOTH RETURN UNDEFINED
+let customers: number[] = [1 ,2 , 3]
+console.log(customers[10])
+console.log(customers?.[10])
+
+//Optional call
+let log: any = null
+
+//Only calls log if it is actually referencing a function using ?
+log?.('a')
